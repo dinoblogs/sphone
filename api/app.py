93ea6acd95aa30ac876a595e068a6b9e5b0d6adf
dinoblogs <113527802+dinoblogs@users.noreply.github.com>
2023-06-21 12:14:32 +0530

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,198 @@
+from flask import Flask, render_template, request, redirect, session
+
+from flask_sqlalchemy import SQLAlchemy
+from fileinput import filename
+import requests
+import cloudinary
+import cloudinary.uploader
+from datetime import datetime
+
+
+cloudinary.config( 
+  cloud_name = "dofvicxek", 
+  api_key = "465383777877424", 
+  api_secret = "a6ErazY7iy9WCT-cneQMZA7sZZQ" 
+)
+
+app = Flask(__name__)
+app.secret_key = "krishna4704"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://townend:krishna4704@db4free.net/townend"
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(800), nullable=False)
+    SP = db.Column(db.String(100), nullable=False)
+    MRP = db.Column(db.String(100), nullable=False)
+    img_url = db.Column(db.String(400), nullable=False)
+    rank = db.Column(db.Integer(), default=1)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f'{self.sno}, {self.title},{self.MRP},{self.SP}, {self.img_url}'
+    
+
+@app.route('/')
+def home():
+    # product = Product(title="Vivo 11", desc="4GB Ram", img_url="http://tou.com", SP='12,000',MRP='15,000', rank=10)
+    # db.session.add(product)
+    # db.session.commit()
+
+    dbms = Product.query.all()
+    n = []
+    if dbms == n or dbms == '':
+        return render_template('index.html',error = 'Sorry No Items Available...😢', dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+    
+
+    return render_template('index.html',error="Our Latest Products...", dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+
+
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/add')
+def create():
+    return render_template('add_item.html')
+
+# Post Request
+@app.route('/create', methods=['POST'])
+def handle_post():
+    f = request.files['img']
+    name1 = str(datetime.now())
+    name1 = name1.replace("-", "")
+    name1 = name1.replace(" ", "")
+    name1 = name1.replace(":", "")
+    name1 = name1.replace(".", "")
+    f.save(f'static/{name1}.png')
+    cloudinary.uploader.upload(f"static/{name1}.png", 
+                               public_id = name1)  
+    #file
+    image_url = f'https://res.cloudinary.com/dofvicxek/image/upload/c_thumb,h_250,w_216/{name1}.png'
+
+    title = request.form['name']
+    sp = request.form['sp']
+    rank = request.form['rank']
+    mrp = request.form['mrp']
+    desc = request.form['desc']
+    product = Product(title=title, desc=desc, img_url=f'{image_url}',rank = rank, SP=sp,MRP=mrp)
+    db.session.add(product)
+    db.session.commit()
+    return redirect('/add')
+
+@app.route('/del', methods=['GET'])
+def del_phone():
+    if request.method == 'GET':
+        sno = request.args['sno']
+        Product.query.filter_by(sno=sno).delete()
+        db.session.commit()
+        return redirect('/admin')
+    else:
+        return 'Error With Server!'
+
+@app.route('/item/')
+def red():
+    return redirect('/')
+@app.route('/item/<name>')
+def items(name):
+    dbms = Product.query.all()
+    for i in dbms:
+        sno = str(i.sno)
+        name = str(name)
+        if sno == name:
+            return render_template('item.html',color=None, name = i.title ,url = i.img_url, desc = i.desc, MRP = i.MRP, SP=i.SP, rank = i.rank)
+            break
+
+        else:
+            pass
+
+    return redirect("/")
+
+
+@app.route("/edit/<name1>")
+def edit(name1):
+    i  = Product.query.filter_by(sno=name1).first()
+    return render_template("edit.html", site=name1,name = i.title ,url = i.img_url, desc = i.desc, MRP = i.MRP, SP=i.SP, rank = i.rank)
+    # n.title = "Iphone"
+    # db.session.commit()
+@app.route("/change/<name>", methods=['POST'])
+def change(name):
+        #file
+    title = request.form['name']
+    sp = request.form['sp']
+    rank = request.form['rank']
+    mrp = request.form['mrp']
+    desc = request.form['desc']
+    i  = Product.query.filter_by(sno=name).first()
+    i.title = title
+    i.SP = sp
+    i.MRP = mrp
+    i.desc = desc
+    i.rank = rank
+
+    db.session.commit()
+
+    return redirect('/add')
+
+@app.route("/admin/")
+def admin():
+    if 'username' in session:
+        if session['username'] == 'admin':
+            if session['password'] == 'kanha0003':
+                dbms = Product.query.all()
+                if dbms == []:
+                    return render_template("dashboard.html" , dbms = dbms,error='No Item!!!')
+                else:
+                    return render_template("dashboard.html" , dbms = dbms)
+            else:
+                return render_template('admin-login.html')
+        else:
+            return render_template('admin-login.html')
+
+            
+    else:
+        return render_template('admin-login.html')
+
+@app.route("/auth" , methods=['POST'])
+def auth():
+    session['username'] = request.form["username"]
+    session['password'] = request.form["password"]
+
+    return redirect('/admin')
+
+@app.route("/logout")
+def logout():
+    session.pop('username',None)  
+    session.pop('password',None)
+    return redirect("/admin")  
+
+@app.route("/search", methods=['GET'])
+def search():
+    user = request.args.get('q')
+    if(user == 'none'):
+        dbms = Product.query.all()
+        n = []
+        if dbms == n or dbms == '':
+            return render_template('search.html',query = 'Sorry No Items Available...😢', dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+        
+
+        return render_template('search.html',query="Latest Items", dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+    else:   
+
+        dbms = Product.query.filter_by(title=user).all()
+        dbms2 = Product.query.filter_by(sno=user).all()
+
+        dbms = dbms+dbms2
+        print(type(dbms))
+        query = f'Search Result for "{user}"'
+        if dbms == [] or dbms == '':
+            return render_template('search.html',query=query,error = f'Sorry No phone with term " {user} " 😢', dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+        
+
+        return render_template('search.html',query=query , dbms=dbms , heading = 'Dino Phons' , title="Dino Phones", email="none@gmail.com", phone="8279692610", add="none", desc="This is my site")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
